9c890be8fe8da6c1b0849f714040c57b0dd1e1b6
Pan <Pan2k17@Ans-MacBook-Pro.local>
2022-10-14 15:35:58 -0400

    working on some route layouts

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+
+// route for homepage 
 app.get('/', (req, res) => {
     res.status(200);
     res.send("Welcome to root URL of Server");
@@ -14,3 +16,26 @@ app.listen(PORT, (error) => {
     else
         console.log("Error occurred, server can't start", error);
 });
+
+// some routes we talked about during our meeting 
+// route: buyNFT -> develop route: see other users' nfts + shopping cart 
+app.get('/buyNFT', (req, res) => {
+    res.status(200);
+    res.send("Buy nft pls");
+}); 
+
+// route: sellNFT -> develop route: redirect to current inventory
+// should put option to choose an available nft and sell 
+app.get('/sellNFT', (req,res) => {
+    res.status(200);
+    res.send("sell nft");
+})
+
+//route auction 
+app.get('/auction', (req,res) => {
+    res.status(200);
+    res.send("auction");
+})
+
+
+
