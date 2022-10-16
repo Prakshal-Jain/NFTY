cf99c0e8259931d24c5117a62bb1685eade41c86
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2022-10-16 17:00:13 -0400

    added docker

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./backend/index');
+var usersRouter = require('./backend/routes/users');
 
 var app = express();
 
