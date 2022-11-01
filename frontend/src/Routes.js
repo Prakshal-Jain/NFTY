ad7f3447c8807ffe6e8b6fd9710262ea9bb9e955
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2022-11-01 13:41:05 -0400

    serving both frontend and backend from just 1 command

---
+++ frontend/src/Routes.js
@@ -0,0 +1,13 @@
+import './homepage.css';
+
+function Routes() {
+  return (
+    <div>
+      <div className="full-page-bg">
+        This is a sample homepage served with React. I will update this with the homepage I am building.
+      </div>
+    </div>
+  );
+}
+
+export default Routes;
