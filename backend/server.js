31655903c8ac77d485472ea88e4260efb2967fe4
An <apn2@buffalo.edu>
2022-11-02 23:46:04 -0400

    retry

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path')
-// const mongoose = require('mongoose');
 
 var products = require('./routes/products'); 
 var users = require('./routes/users'); 
@@ -12,8 +11,27 @@ var selling = require('./routes/selling');
 const app = express();
 const PORT = 8000;
 
-// trying to set up database 
-// mongoose.connect('mongodb://localhost:27017/myapp');
+// setting up database
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/database');
+
+// adding user object to database 
+const userSchema = new mongoose.Schema({
+    name: {
+        first: String,
+        last: String
+    }
+}); 
+// compile model 
+const User = mongoose.model('User', userSchema);
+
+// create document 
+const testing = new User({
+    name: {first: "An", last: "Nguyen"}
+}); 
+
+console.log(testing.name); 
+
 
 // setting up route 
 app.use('/products', products); 
@@ -23,11 +41,9 @@ app.use('/purchase', purchase);
 app.use('/selling', selling)
 
 // route for homepage 
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")))
-
-app.get('/', async (req, res) => {
-    res.status(200)
-    res.sendFile(path.join(__dirname, "frontend", "..", "build", "index.html"))
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send("Welcome to root URL of Server");
 });
 
 app.listen(PORT, (error) => {
@@ -39,5 +55,8 @@ app.listen(PORT, (error) => {
 
 
 // kill server -> sudo lsof -i :8000 // kill -9 ID 
+// git branch stuff: git add -A, git commit -m "", git push -u origin [branchname] 
+
+
 
 
